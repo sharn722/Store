9f899b6f0db4778025b6e64902a238e43b3332fa
sharn7n22 <sharn722@gmail.com>
2018-05-22 07:18:59 +0530

    Add project files.

---
+++ Store/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store.Models;
+
+namespace Store.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var customer = new Customer {Name = "Customer 1", Id = Id};
+
+            return View(customer);
+        }
+    }
+}
---
+++ Store/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store.Models;
+using Store.Models.ViewModels;
+
+namespace Store.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (sortBy == null)
+            {
+                sortBy = "Name";
+            }
+
+            return View();
+            //return Content(String.Format("pageIndex={0}", pageIndex));
+        }
+
+        public ActionResult Index()
+        {
+            var movie = new Movie()
+            {
+                Name = "Iron Man"
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1", Id = 1},
+                new Customer {Name = "Customer 2", Id = 2}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+            //return Content("hi");
+        }
+
+        
+
+    }
+}
